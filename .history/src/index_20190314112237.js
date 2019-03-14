14905e85ca79e7566bcf31d257db2fe513b00f2b
Roslan Paul H. NZAMBA NZAMBA <roslan.p.h.nzamba.nzamba@gmail.com>
2019-03-14 11:30:50 +0000

    end create elements

--- .history/src/index_20190314112237.js
+++ .history/src/index_20190314112237.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import {render} from 'react-dom';
+import {render} 'react-dom';
 const node = document.getElementById('root');
 const root = 
-    React.createElement('div', {},
+    React.createElement('div', {}
         React.createElement('h1', {}, "Hello, world!",
             React.createElement('a', {href: 'mailto:mrxxjoshua@gmail.com'},
-                React.createElement('h1', {}, "React in Action"),
+                React.createElement('h1', {}, "React in Action")
                 React.createElement('em', {}, "...and now it really is")
             )
         )
     );
-    render(root, node);
