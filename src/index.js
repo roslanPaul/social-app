7f5c781319d1c2718e6de8908cbbc6841e00d4bd
Kelvin Liang <kelvin9877@gmail.com>
2019-03-15 21:03:45 +0800

    finish 2.10 code but with bugs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ import PropTypes from "prop-types";
 
 const node = document.getElementById("root");
 
+const data = {
+  post: {
+    id: 123,
+    content:
+      "What we hope ever to do with ease, we must first learn to do with diligence. — Samuel Johnson",
+    user: "Mark Thomas"
+  },
+  comments: [
+    {
+      id: 0,
+      user: "David",
+      content: "such. win."
+    },
+    {
+      id: 1,
+      user: "Haley",
+      content: "Love it."
+    },
+    {
+      id: 2,
+      user: "Peter",
+      content: "Who was Samuel Johnson?"
+    },
+    {
+      id: 3,
+      user: "Mitchell",
+      content: "@Peter get off Letters and do your homework"
+    },
+    {
+      id: 4,
+      user: "Peter",
+      content: "@mitchell ok :P"
+    }
+  ]
+};
+
 class Post extends Component {
   render() {
     return React.createElement(
@@ -69,18 +105,131 @@ Comment.propTypes = {
   user: PropTypes.string.isRequired
 };
 
-const App = React.createElement(
-  Post,
-  {
-    id: 1,
-    content: " said: This is a post!",
-    user: "mark"
-  },
-  React.createElement(Comment, {
-    id: 2,
-    user: "bob",
-    content: " commented: wow! how cool!"
-  })
-);
+class CreateComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+      user: ""
+    };
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-render(App, node);
+  handleUserChange(event) {
+    const val = event.target.value;
+    this.setState(() => ({
+      user: val
+    }));
+  }
+
+  handleTextChange(event) {
+    const val = event.target.value;
+    this.setState(() => ({
+      content: val
+    }));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onCommentSubmit({
+      user: this.state.user.trim(),
+      content: this.state.content.trim()
+    });
+
+    this.setState(() => ({
+      user: "",
+      content: ""
+    }));
+  }
+
+  render() {
+    return React.createElement(
+      "form",
+      {
+        className: "createComment",
+        onSubmit: this.handleSubmit
+      },
+      React.createElement("input", {
+        type: "text",
+        placeholder: "Your name",
+        value: this.state.user,
+        onChange: this.handleUserChange
+      }),
+      React.createElement("input", {
+        type: "text",
+        placeholder: "Thoughts?",
+        value: this.state.content,
+        onChange: this.handleTextChange
+      }),
+      React.createElement("input", {
+        type: "submit",
+        value: "Post"
+      })
+    );
+  }
+}
+
+CreateComment.propTypes = {
+  onCommentSubmit: PropTypes.func.isRequired,
+  content: PropTypes.string
+};
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: this.props.comments
+    };
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  handleCommentSubmit(comment) {
+    const comments = this.state.comments;
+
+    comment.id = Date.now();
+    const newComments = comments.concat([comment]);
+    this.setState({
+      comments: newComments
+    });
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "commentBox"
+      },
+      React.createElement(Post, {
+        id: this.props.post.id,
+        content: this.props.post.content,
+        user: this.props.post.user
+      }),
+      this.state.comment.map(function(comment) {
+        return React.cloneElement(Comment, {
+          key: comment.id,
+          id: comment.id,
+          content: comment.content,
+          user: comment.user
+        });
+      }),
+      React.createElement(CreateComment, {
+        onCommentSubmit: this.handleCommentSubmit
+      })
+    );
+  }
+}
+
+CommentBox.propTypes = {
+  post: PropTypes.object,
+  comments: PropTypes.arrayOf(PropTypes.object)
+};
+
+render(
+  React.createElement(CommentBox, {
+    comments: data.comments,
+    post: data.post
+  }),
+  node
+);
